bc374ea071d9dd8c188e353e89c151981b54b961
jiangfeng1124 <jiangfeng1124@gmail.com>
2012-12-15 05:49:17 +0000

    sample generator

---
+++ graphmodels/files/generator/generate.R
@@ -0,0 +1,13 @@
+suppressPackageStartupMessages(library(bigmatrix))
+
+types <- c("hub", "scale-free", "cluster", "band", "block", "random")
+
+for(type in types) {
+  L <- tiger.generator(n = 200, d = 50, graph = type, prob = 0.5)
+  data <- as.matrix(L$data)
+  file.name <- paste("sample-", type, ".csv", sep="")
+  outfile <- file(file.name, open="wt")
+  res <- lapply(1:nrow(data), function(i) writeLines(paste(data[i,], collapse="\t"), con=outfile))
+  close(outfile)
+}
+
